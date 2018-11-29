298c26ab9f89c5618613dca7c4fa5b3f0e23b45a
wangwenjie <kobejiewen@163.com>
2018-11-29 10:12:34 +0800

    组织列表

--- python_crash_course/Chapter_3/3.3.py
+++ python_crash_course/Chapter_3/3.3.py
@@ -1,2 +1,18 @@
-# 组织列表
+# 3.3组织列表
 
+#方法sort()对列表进行永久性排序
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort()
+print(cars)
+cars.sort(reverse=True)
+print(cars)
+
+#函数sorted()对列表进行临时排序
+cars2 = ['bmw', 'audi', 'toyota', 'subaru']
+print(sorted(cars2))
+
+#reverse() 不是指按与字母顺序相反的顺序排列列表元素，而只是反转列表元素的排列顺序
+cars2.reverse()
+print(cars2)
+print(cars2.__len__())
+print(len(cars2))
