f94620b3100c5877469551c3fca46819f272bbf9
wangwenjie <kobejiewen@163.com>
2018-11-29 18:25:19 +0800

    if语句

---
+++ python_crash_course/Chapter_5/5.3.py
@@ -0,0 +1,18 @@
+# if语句
+
+age = 17
+if age >= 18:
+    print("You are old enough to vote!")
+else:
+    print("Sorry, you are too young to vote.")
+
+# elif
+age = 12
+if age < 4:
+    print("Your admission cost is $0.")
+elif age < 18:
+    print("Your admission cost is $5.")
+else:
+    print("Your admission cost is $10.")
+
+# 多个elif
