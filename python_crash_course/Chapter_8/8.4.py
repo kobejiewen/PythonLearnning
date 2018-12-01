3dfcaf8f44cca2e6c1c87d163677d633974790f9
wangwenjie <kobejiewen@163.com>
2018-12-01 17:50:03 +0800

    传递列表

---
+++ python_crash_course/Chapter_8/8.4.py
@@ -0,0 +1,47 @@
+# 传递列表
+
+def greet_users(names):
+    """向列表中的每位用户都发出简单的问候"""
+    for name in names:
+        msg = "Hello, " + name.title() + "!"
+        print(msg)
+usernames = ['hannah', 'ty', 'margot']
+#greet_users(usernames)
+
+# 在函数中修改列表
+unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+while unprinted_designs:
+    current_design = unprinted_designs.pop()
+    print("Printing model: " + current_design)
+    completed_models.append(current_design)
+# 显示打印好的所有模型
+print("\nThe following models have been printed:")
+for completed_model in completed_models:
+    print(completed_model)
+
+# 重写
+def print_models(unprinted_designs, completed_models):
+    """
+    模拟打印每个设计，直到没有未打印的设计为止
+    打印每个设计后，都将其移到列表completed_models中
+    """
+    while unprinted_designs:
+        current_design = unprinted_designs.pop()
+        # 模拟根据设计制作3D打印模型的过程
+        print("Printing model: " + current_design)
+        completed_models.append(current_design)
+def show_completed_models(completed_models):
+    """显示打印好的所有模型"""
+    print("\nThe following models have been printed:")
+    for completed_model in completed_models:
+        print(completed_model)
+
+unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+print_models(unprinted_designs, completed_models)
+show_completed_models(completed_models)
+
+# 禁止函数修改列表
+# 切片表示法[:] 创建列表的副本
+print_models(unprinted_designs[:], completed_models)
