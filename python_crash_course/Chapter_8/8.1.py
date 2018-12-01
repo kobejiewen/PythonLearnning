05f2c6278bfe845979987b09bfc351ed4f6d667a
wangwenjie <kobejiewen@163.com>
2018-12-01 15:40:03 +0800

    定义函数

---
+++ python_crash_course/Chapter_8/8.1.py
@@ -0,0 +1,13 @@
+# 定义函数
+def greet_user():
+    """显示简单的问候语"""
+    print("Hello!")
+greet_user()
+
+# 向函数传递信息
+def greet_user(username):
+    """显示简单的问候语"""
+    print("Hello, " + username.title() + "!")
+greet_user('jesse')
+
+# 实参和形参
