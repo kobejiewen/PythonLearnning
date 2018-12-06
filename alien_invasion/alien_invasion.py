cfcfb6e67dbd8a79b7795b6e4a67d5e47f4f40c3
wangwenjie <707935082@qq.com>
2018-12-06 23:07:31 +0800

    重构:模块game_functions

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,30 +1,28 @@
-import  sys
+import sys
 import pygame
 from settings import Settings
+from ship import Ship
+import game_functions as gf
+
 
 def run_gamne():
     # 初始化游戏并创建一个屏幕对象
     pygame.init()
     ai_setting = Settings()
-    screen = pygame.display.set_mode((ai_setting.screen_width,ai_setting.screen_height))
+    screen = pygame.display.set_mode((ai_setting.screen_width, ai_setting.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    # 设置背景色
-    bg_color = (ai_setting.bg_color)
+    # 创建一艘飞船
+    ship = Ship(screen)
 
     # 开始游戏的主循环
     while True:
-
         # 监视键盘和鼠标事件
-        for event in pygame.event.get():
-            # 玩家单击游戏窗口的关闭按钮时，将检测到pygame.QUIT 事件，而我们调用sys.exit()来退出游戏
-            if event.type == pygame.QUIT:
-                sys.exit()
+        gf.check_events()
 
         # 每次循环都重绘屏幕
-        screen.fill(bg_color)
+        gf.update_screen(ai_setting,screen,ship)
 
-        # 在我们移动游戏元素时， pygame.display.flip() 将不断更新屏幕，以显示元素的新位置，并在原来的位置隐藏元素，从而营造平滑移动的效果
-        pygame.display.flip()
 
 run_gamne()
+
---
+++ python_crash_course/Chapter_12/12.5.py
@@ -0,0 +1,6 @@
+# 重构:模块game_functions
+
+# 函数 check_events()
+
+# 函数 update_screen()
+
