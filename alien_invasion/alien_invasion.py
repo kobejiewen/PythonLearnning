24cefed75f2676248b06a7259a543a52f1f9693b
wangwenjie <kobejiewen@163.com>
2018-12-07 17:48:09 +0800

    射击

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 import game_functions as gf
+from pygame.sprite import Group
 
 
 def run_gamne():
@@ -15,14 +16,18 @@ def run_gamne():
     # 创建一艘飞船
     ship = Ship(ai_setting,screen)
 
+    # 创建一个用于存储子弹的编组
+    bullets = Group()
+
     # 开始游戏的主循环
     while True:
         # 监视键盘和鼠标事件
-        gf.check_events(ship)
+        gf.check_events(ai_setting,screen,ship,bullets)
         ship.update()
+        bullets.update()
 
         # 每次循环都重绘屏幕
-        gf.update_screen(ai_setting,screen,ship)
+        gf.update_screen(ai_setting,screen,ship,bullets)
 
 
 run_gamne()
