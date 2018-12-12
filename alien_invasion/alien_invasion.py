34a0b9159f8551ce2af3664cee5e00bbfa100026
wangwenjie <kobejiewen@163.com>
2018-12-12 16:08:52 +0800

    创建一群外星人

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -10,26 +10,25 @@ from pygame.sprite import Group
 def run_gamne():
     # 初始化游戏并创建一个屏幕对象
     pygame.init()
-    ai_setting = Settings()
-    screen = pygame.display.set_mode((ai_setting.screen_width, ai_setting.screen_height))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    # 创建一艘飞船
-    ship = Ship(ai_setting,screen)
-
-    # 创建一个外星人
-    alien = Alien(ai_setting,screen)
-
-    # 创建一个用于存储子弹的编组
+    # 创建一艘飞船,创建一个用于存储子弹、外星人的编组
+    ship = Ship(ai_settings,screen)
     bullets = Group()
+    aliens = Group()
+
+    # 创建外星人群
+    gf.create_fleet(ai_settings, screen, ship,aliens)
 
     # 开始游戏的主循环
     while True:
         # 监视键盘和鼠标事件
-        gf.check_events(ai_setting,screen,ship,bullets)
+        gf.check_events(ai_settings,screen,ship,bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_setting,screen,ship,alien,bullets)
+        gf.update_screen(ai_settings,screen,ship,aliens,bullets)
 
         # 删除已消失的子弹
         for bullet in bullets:
@@ -37,7 +36,7 @@ def run_gamne():
                 bullets.remove(bullet)
 
         # 每次循环都重绘屏幕
-        gf.update_screen(ai_setting,screen,ship,alien,bullets)
+        gf.update_screen(ai_settings,screen,ship,aliens,bullets)
 
 
 run_gamne()
---
+++ python_crash_course/Chapter_13/13.3.py
@@ -0,0 +1,15 @@
+# 创建一群外星人
+
+# 确定一行可容纳多少个外星人
+"""
+available_space_x = ai_settings.screen_width – (2 * alien_width)
+number_aliens_x = available_space_x / (2 * alien_width)
+"""
+
+# 创建多行外星人
+
+# 创建外星人群
+
+# 重构create_fleet()
+
+# 添加行
