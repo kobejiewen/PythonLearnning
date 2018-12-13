2f6fb901e2c495d038551a249449a08922d134ec
wangwenjie <kobejiewen@163.com>
2018-12-13 16:21:12 +0800

    射杀外星人

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -46,7 +46,29 @@ def update_screen(ai_settings, screen, ship,aliens,bullets):
     # 让最近绘制的屏幕可见
     pygame.display.flip()
 
-def update_bullets(bullets):
+def create_fleet(ai_settings, screen,ship, aliens):
+    """创建外星人群
+    """
+    # 创建一个外星人，并计算一行可容纳多少个外星人,外星人间距为外星人宽度
+    alien = Alien(ai_settings, screen)
+    number_aliens_x = get_number_aliens_x(ai_settings,alien.rect.width)
+    number_rows = get_number_rows(ai_settings, ship.rect.height,alien.rect.height)
+
+    # 创建外星人群
+    for row_number in range(number_rows):
+        for alien_number in range(number_aliens_x):
+            create_alien(ai_settings,screen,aliens,alien_number,row_number)
+
+def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
+    """响应子弹和外星人的碰撞"""
+    # 删除发生碰撞的子弹和外星人
+    collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
+    if len(aliens) == 0:
+        # 删除现有的所有子弹，并创建一个新的外星人群
+        bullets.empty()
+        create_fleet(ai_settings, screen, ship, aliens)
+
+def update_bullets(ai_settings,screen,ship,aliens,bullets):
     """更新子弹的位置，并删除已消失的子弹"""
     # 更新子弹的位置
     bullets.update()
@@ -54,6 +76,7 @@ def update_bullets(bullets):
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
+    check_bullet_alien_collisions(ai_settings,screen,ship,aliens,bullets)
 
 def fire_bullet(ai_settings, screen, ship, bullets):
     """如果还没有到达限制，就发射一颗子弹"""
@@ -82,19 +105,6 @@ def create_alien(ai_settings, screen, aliens, alien_number,row_number):
     alien.rect.x = alien.x
     alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
     aliens.add(alien)
-
-def create_fleet(ai_settings, screen,ship, aliens):
-    """创建外星人群
-    """
-    # 创建一个外星人，并计算一行可容纳多少个外星人,外星人间距为外星人宽度
-    alien = Alien(ai_settings, screen)
-    number_aliens_x = get_number_aliens_x(ai_settings,alien.rect.width)
-    number_rows = get_number_rows(ai_settings, ship.rect.height,alien.rect.height)
-
-    # 创建外星人群
-    for row_number in range(number_rows):
-        for alien_number in range(number_aliens_x):
-            create_alien(ai_settings,screen,aliens,alien_number,row_number)
 
 def change_fleet_direction(ai_settings, aliens):
     """将整群外星人下移，并改变它们的方向"""
