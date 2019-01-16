60f8568c659f5270f9693b8950cdfbc8b8c32fa5
wangwenjie <kobejiewen@163.com>
2019-01-16 14:14:26 +0800

    创建记分牌

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -7,6 +7,7 @@ import game_functions as gf
 from pygame.sprite import Group
 from game_stats import GameStats
 from button import Button
+from scoreboard import  Scoreboard
 
 def run_gamne():
     # 初始化游戏并创建一个屏幕对象
@@ -25,6 +26,7 @@ def run_gamne():
 
     # 创建一个用于存储游戏统计信息的实例
     stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
 
     # 创建play按钮
     play_button = Button(ai_settings, screen, "Play")
@@ -39,7 +41,7 @@ def run_gamne():
             gf.update_bullets(ai_settings,screen,ship,aliens,bullets)
             gf.update_aliens(ai_settings,stats,screen,ship,aliens,bullets)
 
-        gf.update_screen(ai_settings,screen,stats,ship,aliens,bullets,play_button)
+        gf.update_screen(ai_settings,screen,stats,sb,ship,aliens,bullets,play_button)
 
 run_gamne()
 
--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -54,7 +54,7 @@ def check_play_button(ai_settings,screen,stats, play_button,ship,aliens,bullets,
         create_fleet(ai_settings,screen,ship,aliens)
         ship.center_ship()
 
-def update_screen(ai_settings, screen, stats,ship,aliens,bullets,play_button):
+def update_screen(ai_settings, screen, stats,sb,ship,aliens,bullets,play_button):
     """更新屏幕上的图像，并切换到新屏幕"""
     # 每次循环时都重绘屏幕
     screen.fill(ai_settings.bg_color)
@@ -64,6 +64,9 @@ def update_screen(ai_settings, screen, stats,ship,aliens,bullets,play_button):
         bullet.draw_bullet()
     ship.blitme()
     aliens.draw(screen)
+
+    # 显示得分
+    sb.show_score()
 
     # 如果游戏处于非活动状态，就绘制Play按钮
     if not stats.game_active:
